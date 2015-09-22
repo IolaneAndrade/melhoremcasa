0da7dca7d028d9137f6dcbe8c079f7179f314a14
Lucas Brilhante <lucasb@uab.edu>
2015-09-22 20:38:09 -0300

    Creating class: User and Comment

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Comment.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Comment.java
@@ -6,9 +6,37 @@ import java.util.Date;
  * Created by lucas on 9/22/15.
  */
 public class Comment {
-    protected String text;
-    protected Date date;
-    protected User user;
-    
+    protected String textComment;
+    protected Date dateComment;
+    protected User userComment;
 
+    public Comment(String textComment, Date dateComment, User userComment) {
+        this.textComment = textComment;
+        this.dateComment = dateComment;
+        this.userComment = userComment;
+    }
+
+    public String getTextComment() {
+        return textComment;
+    }
+
+    public void setTextComment(String textComment) {
+        this.textComment = textComment;
+    }
+
+    public Date getDateComment() {
+        return dateComment;
+    }
+
+    public void setDateComment(Date dateComment) {
+        this.dateComment = dateComment;
+    }
+
+    public User getUserComment() {
+        return userComment;
+    }
+
+    public void setUserComment(User userComment) {
+        this.userComment = userComment;
+    }
 }
--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/User.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/User.java
@@ -1,7 +1,42 @@
 package mds.gpp.saudeemcasa.model;
 
+import java.util.Date;
+
 /**
  * Created by lucas on 9/22/15.
  */
 public class User {
+    protected String nameUser;
+    protected String emailUser;
+    protected Date birthDateUser;
+
+    public User(String nameUser, String emailUser, Date birthDateUser) {
+        this.nameUser = nameUser;
+        this.emailUser = emailUser;
+        this.birthDateUser = birthDateUser;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public Date getBirthDateUser() {
+        return birthDateUser;
+    }
+
+    public void setBirthDateUser(Date birthDateUser) {
+        this.birthDateUser = birthDateUser;
+    }
 }
