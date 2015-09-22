02c77c6c1ec771863f09a84ba13a774edf003e8c
Lucas Brilhante <lucasb@uab.edu>
2015-09-22 20:27:23 -0300

    creating classes: User and Comment

---
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Comment.java
@@ -0,0 +1,14 @@
+package mds.gpp.saudeemcasa.model;
+
+import java.util.Date;
+
+/**
+ * Created by lucas on 9/22/15.
+ */
+public class Comment {
+    protected String text;
+    protected Date date;
+    protected User user;
+    
+
+}
---
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/User.java
@@ -0,0 +1,7 @@
+package mds.gpp.saudeemcasa.model;
+
+/**
+ * Created by lucas on 9/22/15.
+ */
+public class User {
+}
