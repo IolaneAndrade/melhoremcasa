1c2d4f6e4b0ad6ba4961cad838003502afb9d490
Lucas Brilhante <lucasb@uab.edu>
2015-09-22 20:56:09 -0300

    Adding fields id to classes

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Comment.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Comment.java
@@ -6,14 +6,34 @@ import java.util.Date;
  * Created by lucas on 9/22/15.
  */
 public class Comment {
+    protected int idComment;
     protected String textComment;
     protected Date dateComment;
     protected User userComment;
+    protected Stablishment stablishment;
 
-    public Comment(String textComment, Date dateComment, User userComment) {
+    public Comment(int idComment, String textComment, Date dateComment, User userComment, Stablishment stablishment) {
+        this.idComment = idComment;
         this.textComment = textComment;
         this.dateComment = dateComment;
         this.userComment = userComment;
+        this.stablishment = stablishment;
+    }
+
+    public int getIdComment() {
+        return idComment;
+    }
+
+    public void setIdComment(int idComment) {
+        this.idComment = idComment;
+    }
+
+    public Stablishment getStablishment() {
+        return stablishment;
+    }
+
+    public void setStablishment(Stablishment stablishment) {
+        this.stablishment = stablishment;
     }
 
     public String getTextComment() {
--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/User.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/User.java
@@ -6,6 +6,7 @@ import java.util.Date;
  * Created by lucas on 9/22/15.
  */
 public class User {
+    protected int idUser;
     protected String nameUser;
     protected String emailUser;
     protected Date birthDateUser;
@@ -14,6 +15,15 @@ public class User {
         this.nameUser = nameUser;
         this.emailUser = emailUser;
         this.birthDateUser = birthDateUser;
+        this.idUser = idUser;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
 
     public String getNameUser() {
