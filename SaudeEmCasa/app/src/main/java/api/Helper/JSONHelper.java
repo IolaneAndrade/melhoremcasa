0485edfdfcbdaf60978631291a760580ba0f4d96
Lucas Brilhante <lucasb@uab.edu>
2015-09-27 21:59:08 -0300

    Completing hospitalListFromJson method

--- SaudeEmCasa/app/src/main/java/api/Helper/JSONHelper.java
+++ SaudeEmCasa/app/src/main/java/api/Helper/JSONHelper.java
@@ -12,9 +12,17 @@ import mds.gpp.saudeemcasa.model.Hospital;
  * Created by lucas on 9/27/15.
  */
 public class JSONHelper {
-
+    //list of hospitals to be populated
     private static List<Hospital> hospitalList = new ArrayList<Hospital>();
 
+    /*
+    * @param hospitalJsonList
+    *               It is the list of hospitals in the JSON format
+    *
+    * @return List of hospital object
+    *
+    * @throws JSONException
+    * */
     public static List<Hospital> hospitalListFromJSON(String hospitalJsonList )throws JSONException {
 
         JSONArray jArray = new JSONArray( hospitalJsonList );
@@ -25,16 +33,24 @@ public class JSONHelper {
             for( int index = 0; index < jArray.length(); index++ ) {
 
                 hospital = new Hospital();
-                //TODO change atribute to the atribute in the JSON file
-                hospital.setType(jArray.getJSONObject(index).getString("atribute"));
 
-                hospital.setNumber(jArray.getJSONObject(index).getString("atribute"));
+                hospital.setType(jArray.getJSONObject(index).getString("tipo_sus"));
 
-                hospital.setDistrict(jArray.getJSONObject(index).getString("atribute"));
+                hospital.setCity(jArray.getJSONObject(index).getString("cidade"));
 
-                hospital.setTelephone(jArray.getJSONObject(index).getString("atribute"));
+                hospital.setState(jArray.getJSONObject(index).getString("uf"));
 
-                hospital.setName(jArray.getJSONObject(index).getString("atribute"));
+                hospital.setNumber(jArray.getJSONObject(index).getString("nu_endereco"));
+
+                hospital.setDistrict(jArray.getJSONObject(index).getString("no_bairro"));
+
+                hospital.setTelephone(jArray.getJSONObject(index).getString("nu_telefone"));
+
+                hospital.setName(jArray.getJSONObject(index).getString("no_fantasia"));
+
+                hospital.setAddress(jArray.getJSONObject(index).getString("no_logradouro"));
+
+                hospital.setRate(0);
 
 
                 hospitalList.add(hospital);
