2c8e7872492702a039d00cf0e05fc4a5376418b6
Lucas Brilhante <lucasb@uab.edu>
2015-09-28 23:10:26 -0300

    Creating HTTP request

---
+++ SaudeEmCasa/app/src/main/java/api/Request/HttpConnection.java
@@ -0,0 +1,34 @@
+package api.Request;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by lucas on 9/28/15.
+ */
+public class HttpConnection {
+
+    public static String request( String url ) throws IOException {
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet http = new HttpGet( "http://mkyong.com" );
+        HttpResponse response= client.execute(http);
+        //String json = new String( client.execute( http ));
+        Log.e( response.getEntity().getContent().read()));
+        return "";
+
+
+    }
+}
