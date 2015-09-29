b9923bb78f9371f9e77a38c478f45c5439350225
Pedro Ivo <andradepedroivo@gmail.com>
2015-09-29 15:15:22 -0300

    Updating Java classes, setting inheritance to Activity and overriding onCreate Method

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
@@ -1,7 +1,17 @@
 package mds.gpp.saudeemcasa.view;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import mds.gpp.saudeemcasa.R;
+
 /**
  * Created by freemanpivo on 9/29/15.
  */
-public class DrugStoreList {
+public class DrugStoreList extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drugstore_list_screen);
+    }
 }
--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalList.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalList.java
@@ -1,7 +1,17 @@
 package mds.gpp.saudeemcasa.view;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import mds.gpp.saudeemcasa.R;
+
 /**
  * Created by freemanpivo on 9/29/15.
  */
-public class HospitalList {
+public class HospitalList extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hospital_list_screen);
+    }
 }
