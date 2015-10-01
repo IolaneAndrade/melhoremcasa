ca565b31c528d7b5327fe54f43bedc58f59e9bd3
Iolane Andrade <andrade.icaa@gmail.com>
2015-10-01 19:06:21 -0300

    Creating new DrugStoreList

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
@@ -4,14 +4,39 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import mds.gpp.saudeemcasa.R;
+import mds.gpp.saudeemcasa.adapter.DrugStoreAdapter;
+import mds.gpp.saudeemcasa.adapter.HospitalAdapter;
+import mds.gpp.saudeemcasa.controller.DrugStoreController;
+import mds.gpp.saudeemcasa.model.DrugStore;
 
-/**
- * Created by freemanpivo on 9/29/15.
- */
+    ListView listView;
+    List<DrugStore> listAdapterDrugStore;
+    ArrayList<DrugStore> list;
+    float[] distances;
+
 public class DrugStoreList extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drugstore_list_screen);
+
+        // Initialize and fill list of drugstore
+        listAdapterDrugStore = DrugStoreController.getAllDrugStore();
+        list = (ArrayList<DrugStore>) listAdapterDrugStore;
+        distances = DrugStoreController.getDistance(this, list);
+
+        // Initializing list view
+        listView = (ListView) findViewById(R.id.listView);
+
+        // Initializing new DrugStoreAdapter with list of drugstore
+        if (distances != null) {
+            DrugStoreAdapter adapter = new DrugStoreAdapter(this, list, distances);
+            // Setting adapter to listView
+            listView.setAdapter(adapter);
+        } else {
+            DrugStoreAdapterWhitoutGPS adapter = new DrugStoreAdapterWhitoutGPS(this, list);
+            listView.setAdapter(adapter);
+        }
+
     }
 }
