ad9291b05c8bea45306fb306affc690694b21c50
Iolane Andrade <andrade.icaa@gmail.com>
2015-10-01 01:31:27 -0300

    Adding a new constructor in DrugStore

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
@@ -16,4 +16,19 @@ public class DrugStore extends Stablishment {
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
+
+    public DrugStore(String nameDrugStore, String telephoneDrugStore) {
+        super(nameDrugStore, telephoneDrugStore);
+    }
+
+   // protected String number = "";
+    //protected String district = "";
+
+    //public String getNumber() { return number; }
+
+    //public void setNumber(String number) { this.number = number; }
+
+    //public String getDistrict() { return district; }
+
+    //public void setDistrict(String district) { this.district = district; }
 }
