8f6c21f2dbc08ed98065c11ebc31b20812a27da7
Iolane Andrade <andrade.icaa@gmail.com>
2015-10-01 01:35:36 -0300

    new constructor in DrugStore

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
@@ -21,14 +21,5 @@ public class DrugStore extends Stablishment {
         super(nameDrugStore, telephoneDrugStore);
     }
 
-   // protected String number = "";
-    //protected String district = "";
-
-    //public String getNumber() { return number; }
-
-    //public void setNumber(String number) { this.number = number; }
-
-    //public String getDistrict() { return district; }
-
-    //public void setDistrict(String district) { this.district = district; }
+   
 }
