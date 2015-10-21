57f7b15e1642ea57b3a60609f83f6bbf08d6afc8
Lucas Brilhante <lucasb@uab.edu>
2015-10-21 12:22:58 -0200

    creating classes for new functionality: drugstore_screen.XML and DrugstoreScreen.java

---
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugstoreScreen.java
@@ -0,0 +1,16 @@
+package mds.gpp.saudeemcasa.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import mds.gpp.saudeemcasa.R;
+
+/**
+ * Created by lucas on 10/21/15.
+ */
+public class DrugstoreScreen extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drugstore_list_screen);
+    }
+}
