a4bd330db4e8f1070604ef8ddc59802f324f7fd5
Vinicius Carvalho <vinisilvacar@gmail.com>
2015-10-28 21:46:19 -0200

    Modification TestGPSTracker

--- SaudeEmCasa/app/src/test/java/TestGPSTracker.java
+++ SaudeEmCasa/app/src/test/java/TestGPSTracker.java
@@ -1,13 +1,16 @@
 //Teste para verificar se GPS está ligado
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import api.Helper.DatabaseHelper;
 import mds.gpp.saudeemcasa.helper.GPSTracker;
 
 
@@ -19,15 +22,23 @@ public class TestGPSTracker extends MyAndroidTestCase {
     boolean isGPSEnable = false;
     boolean isNetworkEnable = false;
     boolean canGetLocation = false;
-
-    double longitude;
-    double latitude;
     Location location;
 
-    protected LocationManager locationManager;
+    private DatabaseHelper dbHelper;
 
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 50;
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    protected LocationManager locationManager;
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final float MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        dbHelper = new DatabaseHelper(getContext());
+
+    }
 
     public void testShouldPass() {
         Context context = getContext();
@@ -36,19 +47,29 @@ public class TestGPSTracker extends MyAndroidTestCase {
     }
 
 
-    /*public void testGetLocation(){
-
+    public void testGetLocation(){
+        Context context = getContext();
         GPSTracker gpsTracker = new GPSTracker(getContext());
         System.out.println("locationManager: "+ locationManager);
         System.out.println("context: "+ getContext());
-        System.out.println("location_service: "+ getContext().LOCATION_SERVICE);
+        System.out.println("location_service: "+ Context.LOCATION_SERVICE);
 
-        locationManager = (LocationManager) getContext().getSystemService(getContext().LOCATION_SERVICE);
-        isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-        location.setLongitude(12.3434);
-        location.setLatitude(-15.3738);
+        //locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        // getting GPS status
+        //isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // getting network status
+        //isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        locationManager.setTestProviderLocation(LocationManager.NETWORK_PROVIDER, location);
+        Log.d("Network", "Network");
+        if (locationManager != null) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if (location != null) {
+                location.setLongitude(12.3434);
+                location.setLatitude(-15.3738);
+            }
+        }
 
         assertNotNull(location);
 
@@ -58,7 +79,7 @@ public class TestGPSTracker extends MyAndroidTestCase {
 
         assertTrue(isNetworkEnable);
 
-    } */
+    }
 
     /*public void testCanGetLocation() throws Exception {
 
