f6f724489a746cfb0f408397e5618772d3301069
Iolane Andrade <andrade.icaa@gmail.com>
2015-11-15 15:46:16 -0200

    testing false ways of input

--- SaudeEmCasa/app/src/androidTest/java/Controller/TestDrugStoreController.java
+++ SaudeEmCasa/app/src/androidTest/java/Controller/TestDrugStoreController.java
@@ -11,10 +11,7 @@ import java.util.List;
 
 import api.Exception.ConnectionErrorException;
 import mds.gpp.saudeemcasa.controller.DrugStoreController;
-import mds.gpp.saudeemcasa.helper.GPSTracker;
 import mds.gpp.saudeemcasa.model.DrugStore;
-import mds.gpp.saudeemcasa.model.Stablishment;
-import mds.gpp.saudeemcasa.view.DrugStoreList;
 import mds.gpp.saudeemcasa.view.LoadingScreen;
 
 /**
@@ -39,70 +36,132 @@ public class TestDrugStoreController extends ActivityInstrumentationTestCase2<Lo
 
     }
 
-    public void tearDown(){
+    public void tearDown() {
         drugStoreController.getDrugStoreDao().deleteAllDrugStores();
 
     }
 
-        public void testInitControllerDrugStore() {
+    public void testInitControllerDrugStore() {
 
-            try {
-                drugStoreController.initControllerDrugstore();
-            } catch (IOException e) {
-                fail();
-            } catch (JSONException e) {
-                fail();
-            } catch (ConnectionErrorException e) {
-                fail();
-            }
-            assertTrue(drugStoreController.getAllDrugstores().size() > 0);
-            myActivity.finish();
-
+        try {
+            drugStoreController.initControllerDrugstore();
+        } catch (IOException e) {
+            fail();
+        } catch (JSONException e) {
+            fail();
+        } catch (ConnectionErrorException e) {
+            fail();
         }
-
-        public void testOrganizeListDrugStoreForDistance() throws Exception{
-
-            ArrayList<DrugStore> drugStoreList = new ArrayList<DrugStore>();
-
-            DrugStore drugStore1 = new DrugStore();
-            drugStore1.setLatitude("16.00");
-            drugStore1.setLongitude("48.00");
-            DrugStore drugStore2 = new DrugStore();
-            drugStore2.setLatitude("16.00");
-            drugStore2.setLongitude("60.00");
-            DrugStore drugStore3 = new DrugStore();
-            drugStore3.setLatitude("16.00");
-            drugStore3.setLongitude("58.00");
-            DrugStore drugStore4 = new DrugStore();
-            drugStore4.setLatitude("16.00");
-            drugStore4.setLongitude("73.00");
-            DrugStore drugStore5 = new DrugStore();
-            drugStore5.setLatitude("16.00");
-            drugStore5.setLongitude("49.00");
-
-            drugStoreList.add(drugStore1);
-            drugStoreList.add(drugStore2);
-            drugStoreList.add(drugStore3);
-            drugStoreList.add(drugStore4);
-            drugStoreList.add(drugStore5);
-
-            drugStoreController.setDrugStoreList(drugStoreList);
-            drugStoreController.oganizeListDrugStoreForDistance();
-
-            ArrayList<DrugStore> drugStoreListToCompare = new ArrayList<DrugStore>();
-
-            drugStoreListToCompare.add(drugStore1);
-            drugStoreListToCompare.add(drugStore5);
-            drugStoreListToCompare.add(drugStore3);
-            drugStoreListToCompare.add(drugStore2);
-            drugStoreListToCompare.add(drugStore4);
-
-            assertTrue(drugStoreListToCompare.get(2).getLongitude() == drugStoreController.getAllDrugstores().get(2).getLongitude());
-
-            myActivity.finish();
-
-        }
+        assertTrue(drugStoreController.getAllDrugstores().size() > 0);
+        myActivity.finish();
 
     }
+
+    public void testInitControllerDrugStoreFalse() {
+
+        try {
+            drugStoreController.initControllerDrugstore();
+        } catch (IOException e) {
+            fail();
+        } catch (JSONException e) {
+            fail();
+        } catch (ConnectionErrorException e) {
+            fail();
+        }
+        assertFalse(drugStoreController.getAllDrugstores().size() < 0);
+        myActivity.finish();
+
+    }
+
+
+    public void testOrganizeListDrugStoreForDistance() throws Exception {
+
+        ArrayList<DrugStore> drugStoreList = new ArrayList<DrugStore>();
+
+        DrugStore drugStore1 = new DrugStore();
+        drugStore1.setLatitude("16.00");
+        drugStore1.setLongitude("48.00");
+        DrugStore drugStore2 = new DrugStore();
+        drugStore2.setLatitude("16.00");
+        drugStore2.setLongitude("60.00");
+        DrugStore drugStore3 = new DrugStore();
+        drugStore3.setLatitude("16.00");
+        drugStore3.setLongitude("58.00");
+        DrugStore drugStore4 = new DrugStore();
+        drugStore4.setLatitude("16.00");
+        drugStore4.setLongitude("73.00");
+        DrugStore drugStore5 = new DrugStore();
+        drugStore5.setLatitude("16.00");
+        drugStore5.setLongitude("49.00");
+
+        drugStoreList.add(drugStore1);
+        drugStoreList.add(drugStore2);
+        drugStoreList.add(drugStore3);
+        drugStoreList.add(drugStore4);
+        drugStoreList.add(drugStore5);
+
+        drugStoreController.setDrugStoreList(drugStoreList);
+        drugStoreController.oganizeListDrugStoreForDistance();
+
+        ArrayList<DrugStore> drugStoreListToCompare = new ArrayList<DrugStore>();
+
+        drugStoreListToCompare.add(drugStore1);
+        drugStoreListToCompare.add(drugStore5);
+        drugStoreListToCompare.add(drugStore3);
+        drugStoreListToCompare.add(drugStore2);
+        drugStoreListToCompare.add(drugStore4);
+
+        assertTrue(drugStoreListToCompare.get(2).getLongitude() == drugStoreController.getAllDrugstores().get(2).getLongitude());
+
+        myActivity.finish();
+
+    }
+
+    public void testOrganizeListDrugStoreForDistanceFalse() throws Exception{
+
+        ArrayList<DrugStore> drugStoreList = new ArrayList<DrugStore>();
+
+        DrugStore drugStore1 = new DrugStore();
+        drugStore1.setLatitude("16.00");
+        drugStore1.setLongitude("48.00");
+        DrugStore drugStore2 = new DrugStore();
+        drugStore2.setLatitude("16.00");
+        drugStore2.setLongitude("60.00");
+        DrugStore drugStore3 = new DrugStore();
+        drugStore3.setLatitude("16.00");
+        drugStore3.setLongitude("58.00");
+        DrugStore drugStore4 = new DrugStore();
+        drugStore4.setLatitude("16.00");
+        drugStore4.setLongitude("73.00");
+        DrugStore drugStore5 = new DrugStore();
+        drugStore5.setLatitude("16.00");
+        drugStore5.setLongitude("49.00");
+
+        drugStoreList.add(drugStore1);
+        drugStoreList.add(drugStore2);
+        drugStoreList.add(drugStore3);
+        drugStoreList.add(drugStore4);
+        drugStoreList.add(drugStore5);
+
+        drugStoreController.setDrugStoreList(drugStoreList);
+        drugStoreController.oganizeListDrugStoreForDistance();
+
+        ArrayList<DrugStore> drugStoreListToCompare = new ArrayList<DrugStore>();
+
+        drugStoreListToCompare.add(drugStore1);
+        drugStoreListToCompare.add(drugStore5);
+        drugStoreListToCompare.add(drugStore3);
+        drugStoreListToCompare.add(drugStore2);
+        drugStoreListToCompare.add(drugStore4);
+
+        assertFalse(drugStoreListToCompare.get(3).getLongitude() == drugStoreController.getAllDrugstores().get(2).getLongitude());
+
+        myActivity.finish();
+
+    }
+
+}
+
+
 
 
--- SaudeEmCasa/app/src/androidTest/java/Controller/TestHospitalController.java
+++ SaudeEmCasa/app/src/androidTest/java/Controller/TestHospitalController.java
@@ -35,9 +35,10 @@ public class TestHospitalController extends ActivityInstrumentationTestCase2<Loa
 
         }
 
-        public void tearDown(){
-            hospitalController.getAllHospitals().deleteAllHospitals();
-        }
+    public void tearDown(){
+        hospitalController.getHospitalDao().deleteAllHospitals();
+
+    }
 
         public void testInitControllerHospital() {
 
@@ -51,6 +52,22 @@ public class TestHospitalController extends ActivityInstrumentationTestCase2<Loa
                 fail();
             }
             assertTrue(hospitalController.getAllHospitals().size() > 0);
+            myActivity.finish();
+
+        }
+
+        public void testInitControllerHospitalFalse() {
+
+            try {
+                hospitalController.initControllerHospital();
+            } catch (IOException e) {
+                fail();
+            } catch (JSONException e) {
+                fail();
+            } catch (ConnectionErrorException e) {
+                fail();
+            }
+            assertFalse(hospitalController.getAllHospitals().size() < 0);
             myActivity.finish();
 
         }
@@ -97,5 +114,47 @@ public class TestHospitalController extends ActivityInstrumentationTestCase2<Loa
         myActivity.finish();
 
     }
-    
+
+    public void testOrganizeListHospitalForDistanceFalse() throws Exception{
+
+        ArrayList<Hospital> hospitalArrayList = new ArrayList<Hospital>();
+
+        Hospital hospital1 = new Hospital();
+        hospital1.setLatitude("16.00");
+        hospital1.setLongitude("48.00");
+        Hospital hospital2 = new Hospital();
+        hospital2.setLatitude("16.00");
+        hospital2.setLongitude("60.00");
+        Hospital hospital3 = new Hospital();
+        hospital3.setLatitude("16.00");
+        hospital3.setLongitude("58.00");
+        Hospital hospital4 = new Hospital();
+        hospital4.setLatitude("16.00");
+        hospital4.setLongitude("73.00");
+        Hospital hospital5 = new Hospital();
+        hospital5.setLatitude("16.00");
+        hospital5.setLongitude("49.00");
+
+        hospitalArrayList.add(hospital1);
+        hospitalArrayList.add(hospital2);
+        hospitalArrayList.add(hospital3);
+        hospitalArrayList.add(hospital4);
+        hospitalArrayList.add(hospital5);
+
+        hospitalController.setHospitalList(hospitalArrayList);
+        hospitalController.oganizeListHospitalForDistance();
+
+        ArrayList<Hospital> hospitalListToCompare = new ArrayList<Hospital>();
+
+        hospitalListToCompare.add(hospital1);
+        hospitalListToCompare.add(hospital5);
+        hospitalListToCompare.add(hospital3);
+        hospitalListToCompare.add(hospital2);
+        hospitalListToCompare.add(hospital4);
+
+        assertFalse(hospitalListToCompare.get(3).getLongitude() == hospitalController.getAllHospitals().get(2).getLongitude());
+
+        myActivity.finish();
+
+    }
 }
