9bda14f0ac28ef4971144c588fbf4ba02063b1cf
freemanpivo <andradepedroivo@gmail.com>
2015-11-17 18:39:51 -0200

    adding if statement for gps error on choose screen

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/ChooseScreen.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/ChooseScreen.java
@@ -5,16 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import mds.gpp.saudeemcasa.R;
+import mds.gpp.saudeemcasa.helper.GPSTracker;
 
 /**
  * Created by freemanpivo on 9/20/15.
  */
 public class ChooseScreen extends Activity{
 
-    public boolean hospitalButtonState = false;
-    public boolean drugStoreButtonState = false;
+    GPSTracker gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +24,18 @@ public class ChooseScreen extends Activity{
 
         Button hospitalButton = (Button) findViewById(R.id.melhor_em_casa_button);
         Button drugStoreButton = (Button) findViewById(R.id.farm_popular_button);
-
+        gps = new GPSTracker(this);
 
         hospitalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextScreen = new Intent(getBaseContext(), HospitalList.class);
-                startActivity(nextScreen);
+                if(gps.canGetLocation()) {
+                    Intent nextScreen = new Intent(getBaseContext(), HospitalList.class);
+                    startActivity(nextScreen);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Ligue seu GPS/Conecte-se a internet",Toast.LENGTH_LONG).show();
+
+                }
 
             }
         });
@@ -37,34 +43,13 @@ public class ChooseScreen extends Activity{
         drugStoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextScreen = new Intent(getBaseContext(), DrugStoreList.class);
-                startActivity(nextScreen);
-            }
-        });
-    }
+                if(gps.canGetLocation()) {
+                    Intent nextScreen = new Intent(getBaseContext(), DrugStoreList.class);
+                    startActivity(nextScreen);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Ligue seu GPS/Conecte-se a internet",Toast.LENGTH_LONG).show();
 
-    private void setupHospitalButton(Button hospitalButton){
-        //1. Get a reference to the button
-        //Button hospitalButton = (Button) findViewById(R.id.melhor_em_casa_button);
-
-        //2. Set the click listener to run the code
-        hospitalButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View screen) {
-                Intent nextScreen = new Intent(getBaseContext(), HospitalList.class);
-                startActivity(nextScreen);
-            }
-        });
-    }
-
-    private void setupDrugStoreButton(Button drugStoreButton){
-        //1. Get a reference to the button
-        //Button drugStoreButton = (Button) findViewById(R.id.drugbutton);
-
-        //2. Set the click listener to run the code
-        drugStoreButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View screen){
-                Intent nextScreen = new Intent(getBaseContext(), DrugStoreList.class);
-                startActivity(nextScreen);
+                }
             }
         });
     }
