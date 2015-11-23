94c0d0bb746e7235dfbbf67e01758c097e15fe7f
Iolane Andrade <andrade.icaa@gmail.com>
2015-11-23 15:02:30 -0200

    google maps - setUps

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalScreen.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalScreen.java
@@ -3,13 +3,18 @@ package mds.gpp.saudeemcasa.view;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.wearable.view.DismissOverlayView;
 import android.text.Html;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import mds.gpp.saudeemcasa.R;
 import mds.gpp.saudeemcasa.controller.HospitalController;
@@ -18,82 +23,168 @@ import mds.gpp.saudeemcasa.controller.HospitalController;
 /**
  * Created by freemanpivo on 11/14/15.
  */
-public class HospitalScreen extends FragmentActivity {
+public class HospitalScreen extends FragmentActivity /*implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener */{
 
-    SupportMapFragment fragmentMap;
-    GoogleMap googleMap;
+    private GoogleMap mMap;
 
-    HospitalController controller = HospitalController.getInstance(this);
-    LatLng originLocation;
-
-
-    @Override
-
-    public void onResume() {
-        super.onResume();
-
-        new Thread(){
-            public void run() {
-                while (fragmentMap.getMap() == null);
-                try {
-                    runOnUiThread (new Runnable() {
-                        @Override
-                        public void run() {
-                            configMap();
-                        }
-                    });
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }.start();
-
-    }
-
-    public void configMap() {
-        googleMap = fragmentMap.getMap();
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        controller.updateGoogleMap();
-    }
+   /* public HospitalScreen(DismissOverlayView mDismissOverlay) {
+        this.mDismissOverlay = mDismissOverlay;
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.hospital_screen);
+        HospitalController controller = HospitalController.getInstance(this);
 
-        SupportMapFragment fragment = SupportMapFragment.newInstance();
-                getSupportFragmentManager().findFragmentById(R.id.hospital_map);
+        setContentView(R.layout.activity_hospital_maps);
+        setUpMapIfNeeded();
+
+        //setContentView(R.layout.hospital_map);
+
+        /*
+        SupportMapFragment fragment = (SupportMapFragment)
+
+        getSupportFragmentManager().findFragmentById(R.id.hospital_map);
+        fragment.getMapAsync(this);
+
+        mDismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
+        mDismissOverlay.setIntroText("ERROR");
+        mDismissOverlay.showIntroIfNecessary();
+
 
         //googleMap = fragment.getMap();
         //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
         //originLocation = new LatLng(-23.561706, -46.655981);
-       // controller.updateGoogleMap();
+        // controller.updateGoogleMap();
+        */
 
-
-        setContentView(R.layout.hospital_screen);
-        HospitalController controller = HospitalController.getInstance(this);
-        
         // setting name
-        TextView nameTextView = (TextView)findViewById(R.id.textViewHospName);
+        TextView nameTextView = (TextView) findViewById(R.id.textViewHospName);
         nameTextView.setText(controller.getHospital().getName());
+
         // Address
-        TextView addressTextView = (TextView)findViewById(R.id.textViewAddressHosp);
-        addressTextView.setText(Html.fromHtml(controller.getHospital().getAddress() + " - " + controller.getHospital().getCity() + " - " + controller.getHospital().getState()));
+        TextView addressTextView = (TextView) findViewById(R.id.textViewAddressHosp);
+        addressTextView.setText(Html.fromHtml(controller.getHospital().
+
+                        getAddress()
+
+                        + " - " + controller.getHospital().
+
+                        getCity()
+
+                        + " - " + controller.getHospital().
+
+                        getState()
+
+        ));
         // setting telephone
-            TextView telephoneTextView = (TextView) findViewById(R.id.textViewHospTel);
-            telephoneTextView.setText("Tel: " + controller.getHospital().getTelephone());
+        TextView telephoneTextView = (TextView) findViewById(R.id.textViewHospTel);
+        telephoneTextView.setText("Tel: " + controller.getHospital().
+
+                        getTelephone()
+
+        );
 
         //set ratting for drugstore
-        RatingBar ratingBarFinal = (RatingBar)findViewById(R.id.ratingBarFinalHospital);
-        ratingBarFinal.setRating(controller.getHospital().getRate());
+        RatingBar ratingBarFinal = (RatingBar) findViewById(R.id.ratingBarFinalHospital);
+        ratingBarFinal.setRating(controller.getHospital().
 
-        TextView textViewRate = (TextView)findViewById(R.id.textViewRatingHospital);
-        textViewRate.setText(""+controller.getHospital().getRate());
+                        getRate()
+
+        );
+
+        TextView textViewRate = (TextView) findViewById(R.id.textViewRatingHospital);
+        textViewRate.setText("" + controller.getHospital().
+
+                        getRate()
+
+        );
 
 
     }
 
+    /*
+    private GoogleMap mMap;
+    private static final LatLng SYDNEY = new LatLng(-33.85704, 151.21522);
+    private DismissOverlayView mDismissOverlay;
+
+    @Override
+    public void onMapReady (GoogleMap map){
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Map is ready to be used.
+        mMap = googleMap;
+
+        // Set the long click listener as a way to exit the map.
+        mMap.setOnMapLongClickListener(this);
+
+        // Add a marker with a title that is shown in its info window.
+        mMap.addMarker(new MarkerOptions().position(SYDNEY)
+                .title("Sydney Opera House"));
+
+        // Move the camera to show the marker.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 10));
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        mDismissOverlay.show();
+
+    }*/
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
 }
+
+
--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/MapToHospital.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/MapToHospital.java
@@ -7,16 +7,56 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mds.gpp.saudeemcasa.R;
+import mds.gpp.saudeemcasa.controller.HospitalController;
 
 public class MapToHospital extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState) {
 
-        View view = inflater.inflate(R.layout.hospital_map, null);
+        View view = inflater.inflate(R.layout.activity_hospital_maps, null);
 
-        return(view);
+
+    /*public void onResume() {
+        super.onResume();
+
+        new Thread() {
+            public void run() {
+                while (fragmentMap.getMap() == null) ;
+                try {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            configMap();
+                        }
+                    });
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }.start();
+
+    }*/
+
+        //public void configMap() {
+        // googleMap = fragmentMap.getMap();
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // controller.updateGoogleMap();
+        //}
+
+
+        return (view);
     }
-
 }
+
