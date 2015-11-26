bdba5a48a8af23b8308c99607084c0449101cdd1
Lucas Brilhante <lucasb@uab.edu>
2015-11-26 19:17:10 -0200

    fixing bug

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugstoreScreen.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugstoreScreen.java
@@ -36,32 +36,23 @@ public class DrugstoreScreen extends Activity {
         // setting telephone
         if(controller.getDrugstore().getType().equals("FARMACIAPOPULAR")){
             TextView telephoneTextView = (TextView) findViewById(R.id.textViewDrugTel);
-            telephoneTextView.setText("");
+            telephoneTextView.setText("Não possui telefone");
         }else {
             TextView telephoneTextView = (TextView) findViewById(R.id.textViewDrugTel);
             telephoneTextView.setText("Tel: " + controller.getDrugstore().getTelephone());
+            setPhoneCallListenner(controller.getDrugstore().getTelephone());
         }
-        setPhoneCallListenner(controller.getDrugstore().getTelephone());
+
     }
 
     private void setPhoneCallListenner(final String telephone) {
         ImageButton phoneCallButton = (ImageButton) findViewById(R.id.phonecallButtonDrugstore);
         phoneCallButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) throws SecurityException{
 
                 Intent phoneCall = new Intent(Intent.ACTION_CALL);
-                phoneCall.setData(Uri.parse(telephone));
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
+                phoneCall.setData(Uri.parse("tel:"+telephone));
                 startActivity(phoneCall);
 
             }
--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalScreen.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/HospitalScreen.java
@@ -44,20 +44,11 @@ public class HospitalScreen extends Activity {
         ImageButton phoneCallButton = (ImageButton) findViewById(R.id.phonecallButtonHospital);
         phoneCallButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) throws SecurityException{
 
-                Intent phoneCall = new Intent(Intent.ACTION_CALL);
-                phoneCall.setData(Uri.parse(telephone));
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
+                Intent phoneCall = new Intent(Intent.ACTION_CALL,Uri.parse("tel:"+telephone));
+
+
                 startActivity(phoneCall);
 
             }
